5ece2fcc3f5fd09c7c4294ce68f54a7d64cf4923
Kipkorir74 <grahamhim74@gmail.com>
2023-10-16 22:39:53 +0300

    setting up mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 
-const express = require('express');
+const express = require('express')
+const mongoose = require('mongoose')
 const TiziRoutes = require('./routes/workouts')
 
 //store the express app in "app"
@@ -10,16 +11,25 @@ const app = express()
 app.use(express.json())
 
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
-//react to requests. Set up a route handler
-app.use('/api/tizi',TiziRoutes)
+//routes
+app.use('/api/tizi', TiziRoutes)
 
-//listen for requests 
-app.listen(process.env.PORT, () => {
-    console.log("Listening to port 4000!!")
-})
+//connect to DB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen for requests 
+        app.listen(process.env.PORT, () => {
+            console.log("Connected to mongoDb and Listening to port", process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
 
