87580992eed72bd9296e16e2d290a62b9aa77dd8
Kipkorir74 <vincentmuthee1@gmail.com>
2023-10-30 16:11:10 +0300

    Fix in single item fetch

--- backend/controllers/workOutController.js
+++ backend/controllers/workOutController.js
@@ -1,4 +1,5 @@
 const workoutModel = require('../models/workoutModel')
+const mongoose = require ('mongoose')
 
 //Retrieve all workouts
 const getAllWorkouts = async(req, res) => {
@@ -10,6 +11,10 @@ const getAllWorkouts = async(req, res) => {
 //Retrieve a single workout
 const getWorkout = async(req,res)=>{
     const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:'Object Not found'})
+    }
 
     const workout = await workoutModel.findById(id)
 
