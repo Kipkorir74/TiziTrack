029b2572c142c72816c4cd7c4a63619fb55ca89f
Kipkorir74 <grahamhim74@gmail.com>
2023-11-02 12:19:09 +0300

    retrieve all data

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 
 const Home = () => {
-  return (
-    <div className='home'>
-      <h1>Halla Madrid</h1>
-    </div>
-  )
+    const [workouts, setWorkouts] = useState(null);
+
+    //The use effect fires only once upon loadng, thus the empty array
+    useEffect(() => {
+        const retrieveWorkouts = async () => {
+            const response = await fetch('/api/tizi/')
+            const json = response.json()
+
+            if (response.ok) {
+                setWorkouts(json)
+            }
+        }
+        retrieveWorkouts()
+    }, [])
+    return (
+        <div className='home'>
+            <div className='workouts'>
+                {
+                    workouts && workouts.length >0 ?
+                    workouts.map((workout) => (
+                    <p key={workout._id}>{workout.title}</p>
+                ))
+                : "No data available"}
+            </div>
+        </div>
+    )
 }
 
 export default Home
