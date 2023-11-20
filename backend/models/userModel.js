e278c6a0e5e52fe0aafc138585a4d6bade8c8000
Kipkorir74 <grahamhim74@gmail.com>
2023-11-20 17:25:59 +0300

    password hashing and email validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,8 @@
 // imports the Mongoose library and makes it available for use
 const mongoose  = require('mongoose')
 
+const bcrypt = require('bcrypt')
+
 // creates a reference to the Schema constructor provided by Mongoose. The schema defines the structure and properties of documents stored in MongoDB collection
 const Schema = mongoose.Schema;
 
@@ -19,5 +21,27 @@ const userSchema = new Schema({
         required:true
     }
 }, {timestamps:true})
+
+// static signup method
+userSchema.static.signup = async function(name, email, password){
+
+    // When using this keyword, do not use arrow function
+    const exists = await this.findOne({email})
+
+    if (exists){
+        throw Error('Email already exists')
+    }
+
+    //create a Hash to increase security incasse of attack
+    const salt = await bcrypt.genSalt(10)
+
+    //Hash password to increase security incasse of attack
+    const hash = await bcrypt.hash(password,salt)
+
+    const user = await this.create({name, email, password: hash})
+
+    return user
+
+}
 
 module.exports = mongoose.model('User', userSchema)
