edf02f5c9c79cb6498241481f974d121aaed58a1
Kipkorir74 <grahamhim74@gmail.com>
2023-11-20 16:48:29 +0300

    changes

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 
 //register routes
 app.use('/api/tizi', TiziRoutes)
-app.use('/app/user/', userRoutes)
+app.use('/api/user/', userRoutes)
 
 //connect to DB
 mongoose.connect(process.env.MONGO_URI)
